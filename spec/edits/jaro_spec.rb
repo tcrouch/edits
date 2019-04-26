05f5fd68f173832ee6ef2931b9bf10dfc779d448
Tom Crouch <tom.crouch@gmail.com>
2019-04-26 20:25:43 +0100

    spec consistency

--- spec/edits/jaro_spec.rb
+++ spec/edits/jaro_spec.rb
@@ -4,22 +4,22 @@ require "spec_helper"
 
 RSpec.describe Edits::Jaro do
   describe ".jaro_matches" do
-    {
-      ["", ""] => [0, 0],
-      ["", "a"] => [0, 0],
-      ["a", ""] => [0, 0],
+    [
+      ["", "", [0, 0]],
+      ["", "a", [0, 0]],
+      ["a", "", [0, 0]],
 
-      %w[abc abc] => [3, 0],
-      %w[abc def] => [0, 0],
-      %w[abcvwxyz cabvwxyz] => [8, 1],
-      %w[dixon dicksonx] => [4, 0],
-      %w[dwayne duane] => [4, 0],
-      %w[information informant] => [9, 1],
-      %w[iota atom] => [2, 1],
-      %w[jones johnson] => [4, 0],
-      %w[martha marhta] => [6, 1],
-      %w[necessary nessecary] => [9, 2]
-    }.each do |(a, b), result|
+      ["abc", "abc", [3, 0]],
+      ["abc", "def", [0, 0]],
+      ["abcvwxyz", "cabvwxyz", [8, 1]],
+      ["dixon", "dicksonx", [4, 0]],
+      ["dwayne", "duane", [4, 0]],
+      ["information", "informant", [9, 1]],
+      ["iota", "atom", [2, 1]],
+      ["jones", "johnson", [4, 0]],
+      ["martha", "marhta", [6, 1]],
+      ["necessary", "nessecary", [9, 2]]
+    ].each do |(a, b, result)|
       context "with '#{a}', '#{b}'" do
         it "returns #{result.first} matches" do
           matches = Edits::Jaro.send(:jaro_matches, a, b).first
@@ -37,22 +37,22 @@ RSpec.describe Edits::Jaro do
   describe ".similarity" do
     subject { described_class.similarity(a, b).round(3) }
 
-    {
-      ["", ""] => 1,
-      ["", "a"] => 0,
-      ["a", ""] => 0,
+    [
+      ["", "", 1],
+      ["", "a", 0],
+      ["a", "", 0],
 
-      %w[abc abc] => 1,
-      %w[abc def] => 0,
-      %w[abcvwxyz cabvwxyz] => 0.958,
-      %w[dixon dicksonx] => 0.767,
-      %w[dwayne duane] => 0.822,
-      %w[information informant] => 0.902,
-      %w[iota atom] => 0.5,
-      %w[jones johnson] => 0.790,
-      %w[martha marhta] => 0.944,
-      %w[necessary nessecary] => 0.926
-    }.each do |(a, b), expected|
+      ["abc", "abc", 1],
+      ["abc", "def", 0],
+      ["abcvwxyz", "cabvwxyz", 0.958],
+      ["dixon", "dicksonx", 0.767],
+      ["dwayne", "duane", 0.822],
+      ["information", "informant", 0.902],
+      ["iota", "atom", 0.5],
+      ["jones", "johnson", 0.790],
+      ["martha", "marhta", 0.944],
+      ["necessary", "nessecary", 0.926]
+    ].each do |(a, b, expected)|
       context "with '#{a}', '#{b}'" do
         let(:a) { a }
         let(:b) { b }
--- spec/edits/jaro_winkler_spec.rb
+++ spec/edits/jaro_winkler_spec.rb
@@ -6,27 +6,49 @@ RSpec.describe Edits::JaroWinkler do
   describe ".similarity" do
     subject { described_class.similarity(a, b).round(3) }
 
-    {
-      ["", ""] => 1,
-      ["", "a"] => 0,
-      ["a", ""] => 0,
+    [
+      ["", "", 1],
+      ["", "a", 0],
+      ["a", "", 0],
 
-      %w[abc abc] => 1,
-      %w[abc def] => 0,
-      %w[abcvwxyz cabvwxyz] => 0.958,
-      %w[dixon dicksonx] => 0.813,
-      %w[dwayne duane] => 0.84,
-      %w[information informant] => 0.941,
-      %w[iota atom] => 0.5,
-      %w[jones johnson] => 0.832,
-      %w[martha marhta] => 0.961,
-      %w[necessary nessecary] => 0.941
-    }.each do |(a, b), expected|
+      ["abc", "abc", 1],
+      ["abc", "def", 0],
+      ["abcvwxyz", "cabvwxyz", 0.958],
+      ["dixon", "dicksonx", 0.813],
+      ["dwayne", "duane", 0.84],
+      ["information", "informant", 0.941],
+      ["iota", "atom", 0.5],
+      ["jones", "johnson", 0.832],
+      ["martha", "marhta", 0.961],
+      ["necessary", "nessecary", 0.941]
+    ].each do |(a, b, expected)|
       context "with '#{a}', '#{b}'" do
         let(:a) { a }
         let(:b) { b }
 
         it { is_expected.to eq expected }
+      end
+    end
+
+    context "with threshold 0.8" do
+      subject { described_class.similarity(a, b, threshold: 0.8).round(3) }
+
+      context "with 'dixon', 'dicksonx'" do
+        let(:a) { "dixon" }
+        let(:b) { "dicksonx" }
+
+        it { is_expected.to eq 0.767 }
+      end
+    end
+
+    context "with weight 0.2" do
+      subject { described_class.similarity(a, b, weight: 0.2).round(3) }
+
+      context "with 'dixon', 'dicksonx'" do
+        let(:a) { "dixon" }
+        let(:b) { "dicksonx" }
+
+        it { is_expected.to eq 0.86 }
       end
     end
   end
