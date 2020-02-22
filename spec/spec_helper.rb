af016ff3226bc9b4316a4edbaaab7a16682934d6
Tom Crouch <tom.crouch@gmail.com>
2020-02-22 13:13:36 +0000

    Use doc formatter for single spec runs only

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,13 @@ RSpec.configure do |config|
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
+  if config.files_to_run.one?
+    # Use the documentation formatter for detailed output,
+    # unless a formatter has already been configured
+    # (e.g. via a command-line flag).
+    config.default_formatter = "doc"
+  end
+
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
